1cd2db273f5332ef48fc3b91f44d497a9e288031
Vince <vlee9527@gmail.com>
2021-11-30 02:08:55 +0800

    Init - Scss
    Added scss reset file
    Added variables.scss to store all variables
    Removed vue cli default pages

---
+++ src/views/SignIn.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="signin">
+    <h1 class="title">This is SignIn page</h1>
+  </div>
+</template>
+
+<style lang="scss">
+@import "./../assets/styles/_signin.scss";
+</style>
