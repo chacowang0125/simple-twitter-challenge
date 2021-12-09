6a76ea8af63a1add1df3690566cdc5f58ac2449e
chaco <chacowang0125@gmail.com>
2021-12-09 20:56:11 +0800

    deploy

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,31 +1,31 @@
 import axios from 'axios'
 import Swal from 'sweetalert2'
 
-const baseURL = 'http://localhost:3000/api'
+const baseURL = 'https://twitterapi20211207.herokuapp.com/api'
 
 const axiosInstance = axios.create({
-  baseURL
+    baseURL
 })
 
 axiosInstance.interceptors.request.use(
-  config => {
-    // 從 localStorage 將 token 取出
-    const token = localStorage.getItem('token')
+    config => {
+        // 從 localStorage 將 token 取出
+        const token = localStorage.getItem('token')
 
-    // 如果 token 存在的話，則帶入到 headers 當中
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`
-    }
-    return config
-  },
-  err => Promise.reject(err)
+        // 如果 token 存在的話，則帶入到 headers 當中
+        if (token) {
+            config.headers['Authorization'] = `Bearer ${token}`
+        }
+        return config
+    },
+    err => Promise.reject(err)
 )
 
 export const apiHelper = axiosInstance
 
-export const Toast = Swal.mixin ({
-  toast: true,
-  position: 'top-end',
-  showConfirmButton: false,
-  timer: 3000
+export const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 3000
 })
