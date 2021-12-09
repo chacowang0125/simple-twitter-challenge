1144646a0100fbee52949106bacc3cdc1a812e7d
Vince <vlee9527@gmail.com>
2021-12-10 01:23:33 +0800

    fixed admin users display bug

--- src/components/AdminUser.vue
+++ src/components/AdminUser.vue
@@ -1,13 +1,15 @@
 <template>
-  <div class="admin-user-card">
+  <div class="admin-user-card" v-if="user.role !== 'admin'">
     <div class="admin-user-img">
       <img class="admin-user-img cover" :src="user.cover | emptyImage" />
       <img class="admin-user-img avatar" :src="user.avatar | emptyImage" />
     </div>
     <div class="admin-user-content">
       <div class="admin-user-info">
-        <span class="admin-user-info name">{{ user.name | nameLength}}</span>
-        <span class="admin-user-info detail">@{{ user.account | nameLength}}</span>
+        <span class="admin-user-info name">{{ user.name | nameLength }}</span>
+        <span class="admin-user-info detail"
+          >@{{ user.account | nameLength }}</span
+        >
       </div>
       <div class="admin-user-data-wrapper">
         <div class="admin-user-data group">
@@ -15,18 +17,14 @@
             class="admin-user-data icon"
             src="./../assets/images/comment-icon.svg"
           />
-          <span class="admin-user-data number">{{
-            user.tweetCounts
-          }}</span>
+          <span class="admin-user-data number">{{ user.tweetCounts }}</span>
         </div>
         <div class="admin-user-data group">
           <img
             class="admin-user-data icon"
             src="./../assets/images/like-icon.svg"
           />
-          <span class="admin-user-data number">{{
-            user.likeCounts
-          }}</span>
+          <span class="admin-user-data number">{{ user.likeCounts }}</span>
         </div>
       </div>
       <div class="admin-follow-wrapper">
@@ -48,14 +46,14 @@ import { nameLengthFilter } from "./../utils/mixins";
 import { emptyImageFilter } from "./../utils/mixins";
 
 export default {
-  mixins: [ emptyImageFilter,nameLengthFilter],
+  mixins: [emptyImageFilter, nameLengthFilter],
   name: "AdminUser",
   props: {
-		user: {
-			type: Object,
-			require: true
-		}
-	}
+    user: {
+      type: Object,
+      require: true,
+    },
+  },
 };
 </script>
 
