e4ba4f635677ab1bd32dce04aa08410085ea1732
chaco <chacowang0125@gmail.com>
2021-12-09 21:52:19 +0800

    nav

--- src/views/ChatRoom.vue
+++ src/views/ChatRoom.vue
@@ -1,3 +1,17 @@
 <template>
-	<h1>chat room</h1>
+	<div class="container">
+		<Navbar />
+		<h1>chat room</h1>
+  </div>
 </template>
+
+<script>
+import Navbar from "../components/NavBar.vue"
+
+export default({
+	name:"PrivateChat",
+	componrnt: {
+		Navbar
+	}
+})
+</script>
--- src/views/PrivateChat.vue
+++ src/views/PrivateChat.vue
@@ -1,3 +1,16 @@
 <template>
-	<h1>private chat</h1>
+  <div class="container">
+		<Navbar />
+  </div>
 </template>
+
+<script>
+import Navbar from "../components/NavBar.vue"
+
+export default({
+	name:"PrivateChat",
+	componrnt: {
+		Navbar
+	}
+})
+</script>
