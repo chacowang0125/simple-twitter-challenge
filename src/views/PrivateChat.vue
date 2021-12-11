9c47043ea57c9fdee0af65becf141501810969d8
Vince <vlee9527@gmail.com>
2021-12-11 17:08:22 +0800

    private m

--- src/views/PrivateChat.vue
+++ src/views/PrivateChat.vue
@@ -14,7 +14,11 @@
     <div class="message-content">
       <div class="title">聊天對象</div>
       <div class="content">
-        <ChatRoomMessage />
+        <ChatRoomMessage
+          :contents="contents"
+          :logged="logged"
+          @after-send-message="afterSendMessage"
+        />
       </div>
     </div>
   </div>
@@ -24,6 +28,8 @@
 import Navbar from "../components/NavBar.vue";
 import UserListCard from "../components/UserListCard.vue";
 import ChatRoomMessage from "../components/ChatRoomMessage.vue";
+import { mapState } from "vuex";
+import chatAPI from "./../apis/chat";
 
 export default {
   name: "PrivateChat",
@@ -31,6 +37,59 @@ export default {
     Navbar,
     UserListCard,
     ChatRoomMessage,
+  },
+  data() {
+    return {
+      room: "1",
+      loginUser: [],
+      contents: [],
+      content: {},
+      logged: {},
+    };
+  },
+  mounted() {
+    this.$socket.open();
+  },
+  sockets: {
+    connect() {
+      console.log("socket connected");
+      // this.socketConnect();
+      this.$socket.emit("login");
+    },
+    message(data) {
+      this.content = data;
+      this.contents.push(this.content);
+    },
+    loginUser(data) {
+      console.log(data);
+      this.loginUser = data;
+    },
+    loginStatus(data) {
+      this.logged = data;
+      this.contents.push({ online: data });
+    },
+    // disconnected() {
+    //   this.$socket.emit("disconnect", this.currentUser.id);
+    // },
+  },
+  methods: {
+    afterSendMessage(text) {
+      this.$socket.emit("sendMessage", {
+        text: text,
+        id: this.currentUser.id,
+        room: this.room,
+      });
+    },
+    async fetchChatHistory() {
+      const data = await chatAPI.publicHistory();
+      this.contents = data.data;
+    },
+  },
+  computed: {
+    ...mapState(["currentUser", "token"]),
+  },
+  created() {
+    this.fetchChatHistory();
   },
 };
 </script>
@@ -51,14 +110,14 @@ export default {
   }
   .user-list {
     flex: 1;
-		overflow-y: scroll;
+    overflow-y: scroll;
     &::-webkit-scrollbar {
-        width: 3px;
-        background-color: transparent;
-        &-thumb {
-            border-radius: 10px;
-            background-color: $page-divider;
-        }
+      width: 3px;
+      background-color: transparent;
+      &-thumb {
+        border-radius: 10px;
+        background-color: $page-divider;
+      }
     }
   }
   .message-content {
@@ -71,7 +130,6 @@ export default {
     flex-direction: column;
     .content {
       flex: 1;
-
     }
   }
 }
